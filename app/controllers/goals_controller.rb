806ac9482bc3ed8f95274ecc5f63595710398792
ielole <ieloletin@gmail.com>
2017-01-12 20:39:20 -0500

    Set up show functionality for goals.
    - Add before-action, set_goal, goal_params and private.
    - Create show route for an individual goal.
    - Create method for show.

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,7 +1,23 @@
 class GoalsController < ApplicationController
+  before_action :set_goal, only: [:show, :update, :destroy]
+
   def index
     @goals = Goal.all
 
     render json: @goals
   end
+
+  def show
+    render json: Goal.find(params[:id])
+  end
+
+  def set_goal
+    @goal = Goal.find(params[:id])
+  end
+
+  def goal_params
+    params.require(:goal).permit(:title, :start_date, :end_date, :user_id)
+  end
+
+  private :set_goal, :goal_params
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 Rails.application.routes.draw do
   resources :examples, except: [:new, :edit]
   get '/goals' => 'goals#index'
+  get '/goals/:id' => 'goals#show'
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
